ee976e2054f658cab13ffd0acf498067bd66944a
Jennifer Humphrey <jen.e.humphrey@gmail.com>
2015-06-23 19:34:20 -0600

    changed default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'http://g7quizracer.com' }
+  config.action_mailer.default_url_options = { :host => 'http://www.g7quizracer.com' }
 end
